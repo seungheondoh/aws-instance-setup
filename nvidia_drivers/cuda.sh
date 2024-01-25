8a44b399366c493d644cab8bba7782cbea38a803
Ubuntu <ubuntu@ip-172-31-61-240.ec2.internal>
2024-01-25 06:50:31 +0000

    update

--- nvidia_drivers/cuda.sh
+++ nvidia_drivers/cuda.sh
@@ -5,4 +5,10 @@ sudo dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
-cat /usr/local/cuda/version.txt
+sudo systemctl start nvidia-fabricmanager
+cat /usr/local/cuda/version.json
+
+#### If problem 
+# export CUDA_HOME=/usr/local/cuda-12.1
+# export PATH=/usr/local/cuda-12.1/bin${PATH:+:${PATH}}
+# export LD_LIBRARY_PATH=/usr/local/cuda-12.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
